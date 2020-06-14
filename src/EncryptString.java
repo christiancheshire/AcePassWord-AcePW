f97979d22707786fdc056db6593531f3a2da969f
michaelgarcangel <42846285+michaelgarcangel@users.noreply.github.com>
2020-06-13 23:15:30 -1000

    Update EncryptString.java
    Added Strong Text Encryptor instance

--- src/EncryptString.java
+++ src/EncryptString.java
@@ -1,4 +1,10 @@
+import org.jasypt.util.text.StrongTextEncryptor;
 
 public class EncryptString {
-
+	public String encrypt(String password) {
+		String encryptedPW;
+		StrongTextEncryptor encryptor = new StrongTextEncryptor();
+		encryptedPW = encryptor.encrypt(password);
+		return encryptedPW;
+	}
 }
