97b0a62e752277a29d1ac9f4bf2426eb5769679e
christiancheshire <42689574+christiancheshire@users.noreply.github.com>
2020-06-14 16:36:05 -1000

    Fixed commit errors and merge issues.

---
+++ AcePassWord-AcePW/src/Main.java
@@ -0,0 +1,16 @@
+
+/*
+ * Copyright 2020
+ * 3 Musketeers Tech (Michael Arcangel, Caleb Cheshire, Christian Cheshire)
+ * AcePW - ICS 427 Project
+ */
+
+public class Main {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		System.out.println("AcePW - Ace PassWord");
+        AppGUI gui = new AppGUI();
+	}
+	
+}
