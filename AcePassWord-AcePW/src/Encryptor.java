0a31c8e4569a3c1d997a436fec13f71d6ca6c9e2
michaelgarcangel <marcangel2016@gmail.com>
2020-06-14 20:18:26 -1000

    Encryptor.java class added
    Encryptor class with added dependencies for jasypt 1.9.2.
    
    Handles encryption and decryption of user passwords stored locally.
    
    Pending Tests!

---
+++ AcePassWord-AcePW/src/Encryptor.java
@@ -0,0 +1,30 @@
+import org.jasypt.util.text.StrongTextEncryptor;
+/**
+ * Copyright 2020
+ * 3 Musketeers Tech
+ * Encryptor class with imports from Jasypt 1.9.2
+ * 
+ *
+ */
+public class Encryptor {
+	/*
+	 * Encrypts the password to be saved in local data.
+	 */
+	public String encrypt(String password) {
+		String toEncrypt;
+		StrongTextEncryptor encryptor = new StrongTextEncryptor();
+		toEncrypt = encryptor.encrypt(password);
+		return toEncrypt;
+	}
+	
+	/*
+	 * Decrypts the password saved in local data, given the username for
+	 * that particular account.
+	 */
+	public String decrypt(String username) {
+		String toDecrypt;
+		StrongTextEncryptor decryptor = new StrongTextEncryptor();
+		toDecrypt = decryptor.decrypt(username);
+		return toDecrypt;
+	}
+}
